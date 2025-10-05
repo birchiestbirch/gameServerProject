7350fc5e95f29a5b01059dddb9f56902ca96c808
Birch <boarcukmaksim@gmail.com>
2025-10-05 13:08:54 +0300

    semi-major updates

--- test.py
+++ test.py
@@ -5,15 +5,19 @@ from reload import reload
 
 root = CTk()
 root.geometry("500x500")
+root.configure(fg_color="grey30")
 
-start = CTkButton(root, text="START", fg_color="green", command=start)
-start.pack(padx=20, pady=20)
+for i in range(3):root.rowconfigure(weight=i, index=3)
+for r in range(3):root.columnconfigure(weight=r, index=3)
 
-stop = CTkButton(root, text="STOP", fg_color="red", command=stop)
-stop.pack(padx=20, pady=20)
+start_btn = CTkButton(root, text="START", fg_color="#276d30", text_color="#C5C2C2", command=start)
+start_btn.grid(row=3, column=0)
 
-reload = CTkButton(root, text="RELOAD", fg_color="yellow", command=reload)
-reload.pack(padx=20, pady=20)
+stop_btn = CTkButton(root, text="STOP", fg_color="#ac1616", text_color="#C5C2C2", command=stop)
+stop_btn.grid(row=3, column=1)
+
+reload_btn = CTkButton(root, text="RELOAD", fg_color="#bdb21d", text_color="#C5C2C2", command=reload)
+reload_btn.grid(row=3, column=2)
 
 
 root.mainloop()
